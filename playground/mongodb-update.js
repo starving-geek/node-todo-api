78d83cce44a41a637f421aa4dcdd117d16ec629b
Tyler <tyler@geekingdoing.com>
2018-03-17 21:41:56 -0400

    added mongodb-update.js file

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb'); //object destructuring
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID("5aad7aa385316a238dfc34f5")
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID("5aaa45e8099ab807ab3bdda8")
+    }, {
+        $set: {
+            name: 'Tyler'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    })
+   // db.close();
+});
