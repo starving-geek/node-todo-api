d117101ca11fe24bb1682a16d844272438f6b272
Tyler <tyler@geekingdoing.com>
2018-04-09 20:30:55 -0400

    Fixed bug in config file

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ if(env === 'development') {
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if(env === 'test') {
     process.env.PORT = 3000;
-    process.env,MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
